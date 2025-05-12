a325f9043d375bab7882946e295a65f634998289
MattTidd <mtidd2@unb.ca>
2025-05-12 10:26:49 -0300

    get_locations .cpp file

---
+++ sim_bt/src/get_locations_node.cpp
@@ -0,0 +1,46 @@
+#include <sim_bt/get_locations_node.hpp>
+#include <behaviortree_cpp/blackboard.h>
+#include <behaviortree_cpp/bt_factory.h>
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
+
+namespace sim_bt
+{
+    // constructor:
+    GetLocations::GetLocations(const std::string& name, const BT::NodeConfiguration& config)
+    : BT::SyncActionNode(name, config)
+{
+    // get node info from blackboard:
+    auto node = config.blackboard->get<rclcpp::Node*>("node");
+
+    // declare & get a parameter containing the list of poses, flattened as x,y, yaw:
+    node->declare_parameter<std::vector<double>>("task_locations", {});
+    std::vector<double> flat;
+    node->get_parameter("task_locations", flat);
+
+    // convert [x0, y0, yaw0, x1, y1, yaw1, ...] into PoseStamped list:
+    for (size_t i = 0; i+2 < flat.size(); i +=3) {
+        geometry_msgs::msg::PoseStamped p;
+        p.header.frame_id = "map";
+        p.pose.position.x = flat[i];
+        p.pose.position.y = flat[i + 1];
+        tf2::Quaternion q;
+        q.setRPY(0,0, flat[i+2]);
+        p.pose.orientation = tf2::toMsg(q);
+        locations_.push_back(p);
+    }  
+}
+
+BT::NodeStatus GetLocations::tick()
+{
+    setOutput("locations", locations_);
+    return BT::NodeStatus::SUCCESS;
+}
+
+}   // namespace sim_bt
+
+// register with the factory:
+BT_REGISTER_NODES(factory)
+{
+    factory.registerNodeType<sim_bt::GetLocations>("GetLocations");
+}
