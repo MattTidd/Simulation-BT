76d3f6c372b091542159915eb693ebc7dd10959b
MattTidd <mtidd2@unb.ca>
2025-05-13 10:49:11 -0300

    fixed the tree parsing. added debugs. passed node and task count to blackboard for use elsewhere

--- sim_bt/src/main.cpp
+++ sim_bt/src/main.cpp
@@ -3,6 +3,7 @@
 #include "sim_bt/get_locations_node.hpp"
 #include "sim_bt/at_task_n_node.hpp"
 #include "sim_bt/go_to_n_node.hpp"
+#include <ament_index_cpp/get_package_share_directory.hpp>
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
@@ -18,18 +19,29 @@ int main(int argc, char** argv) {
     factory.registerNodeType<sim_bt::GoToN>("GoToN");
     // add the inspection node when it is made!
 
-    // load behavior tree from XML file:
+    // get package path:
+    const std::string package_share_dir = ament_index_cpp::get_package_share_directory("sim_bt");
+
+    // load behavior tree from XML file using absolute path:
     const std::string tree_filename = node->declare_parameter<std::string>(
-      "tree_file", 
-      "trees/main_tree.xml"
+      "tree_file",      // try this
+      package_share_dir + "/trees/my_tree.xml"  // fallback to this
     );
+
+    // get the task_count parameter:
+    int task_count = node->declare_parameter<int>("task_count", 1);
+
+    // debug:
+    RCLCPP_INFO(node->get_logger(), "Loading tree from: %s", tree_filename.c_str());
 
     // create the tree:
     auto blackboard = BT::Blackboard::create();    // create a blackboard
-    blackboard->set("logger", node->get_logger()); // send the logger to the blackboard
+    blackboard->set("node", node.get()); // send the node to the blackboard
+    blackboard->set("task_count", task_count);  // send the number of tasks to the blackboard
+    RCLCPP_INFO(node->get_logger(), "Passed task count: %d", task_count);
     auto tree = factory.createTreeFromFile(tree_filename, blackboard);  // create tree
 
-    // configure tree tick timer
+    // configure tree tick timer:
     const auto tick_period = std::chrono::milliseconds(
       node->declare_parameter<int>("tick_rate_ms", 100)
     );
