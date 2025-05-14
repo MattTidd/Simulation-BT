f47389cb442314527fd4bc9dad97b18a169eb08a
MattTidd <mtidd2@unb.ca>
2025-05-14 11:35:02 -0300

    changed from true condition node to action node dressed as condition

--- sim_bt/include/sim_bt/check_locations_node.hpp
+++ sim_bt/include/sim_bt/check_locations_node.hpp
@@ -1,17 +1,19 @@
 #ifndef SIM_BT__CHECK_LOCATIONS_NODE_HPP_
 #define SIM_BT__CHECK_LOCATIONS_NODE_HPP_
 
-#include <behaviortree_cpp/condition_node.h>
+#include <behaviortree_cpp/action_node.h>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <vector>
 
 namespace sim_bt {
 
-class CheckLocations : public BT::ConditionNode {
+class CheckLocations : public BT::SyncActionNode 
+{
 public:
   CheckLocations(const std::string& name, const BT::NodeConfiguration& config);
 
-  static BT::PortsList providedPorts() {
+  static BT::PortsList providedPorts() 
+  {
     return { BT::InputPort<std::vector<geometry_msgs::msg::PoseStamped>>("locations") };
   }
 
--- sim_bt/src/check_locations_node.cpp
+++ sim_bt/src/check_locations_node.cpp
@@ -1,10 +1,13 @@
 #include "sim_bt/check_locations_node.hpp"
+#include <behaviortree_cpp/blackboard.h>
 #include <behaviortree_cpp/bt_factory.h>
+#include <iostream>
+#include <rclcpp/rclcpp.hpp>
 
 namespace sim_bt {
 
 CheckLocations::CheckLocations(const std::string& name, const BT::NodeConfiguration& config)
-  : BT::ConditionNode(name, config) {}
+  : BT::SyncActionNode(name, config) {}
 
 BT::NodeStatus CheckLocations::tick() {
   std::vector<geometry_msgs::msg::PoseStamped> locations;
